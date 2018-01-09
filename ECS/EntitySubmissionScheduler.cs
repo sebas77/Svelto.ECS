29931c39376d4d100f68b5522aa2a3fab2f6a501
sebas77 <ssense@tiscali.it>
2018-01-09 13:35:06 +0000

    remove unused files after merging from wip

--- ECS/EntitySubmissionScheduler.cs
+++
@@ -1,9 +0,0 @@
-using Svelto.WeakEvents;
-
-namespace Svelto.ECS.Schedulers
-{
-    public abstract class EntitySubmissionScheduler
-    {
-        abstract public void Schedule(WeakAction submitEntityViews);
-    }
-}
