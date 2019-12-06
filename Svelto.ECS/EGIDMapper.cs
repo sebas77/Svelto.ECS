725e124184bac2a39a7889e562a75a4a55984d09
Sebastiano Mandala <ssense@tiscali.it>
2019-12-06 14:11:32 +0000

    removed unecessary files and dependencies that would have forced to use packages in Unity

--- Svelto.ECS/EGIDMapper.cs
+++ Svelto.ECS/EGIDMapper.cs
@@ -32,20 +32,6 @@ namespace Svelto.ECS
             value = default;
             return false;
         }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public UnsafeStructRef<T> EntityUnsafeRef(uint entityID)
-        {
-#if DEBUG && !PROFILER
-            if (map.TryFindIndex(entityID, out var findIndex) == false)
-                throw new Exception("Entity not found in this group ".FastConcat(typeof(T).ToString()));
-#else
-                map.TryFindIndex(entityID, out var findIndex);
-#endif
-            var alloc = GCHandle.Alloc(map.valuesArray, GCHandleType.Pinned);
-            
-            return new UnsafeStructRef<T>(ref map.valuesArray[findIndex], alloc);
-        }
     }
 }
 
