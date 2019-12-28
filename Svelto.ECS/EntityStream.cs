0e83e016c8033b485089a6a4604e6b458061348d
sebas77 <ssense@tiscali.it>
2019-12-28 13:41:32 +0000

    use webgl-friendly EntityStream

--- Svelto.ECS/EntityStream.cs
+++ Svelto.ECS/EntityStream.cs
@@ -95,7 +95,7 @@ namespace Svelto.ECS
             _consumers.UnorderedRemove(consumer);
         }
 
-        readonly ThreadSafeFasterList<Consumer<T>> _consumers = new ThreadSafeFasterList<Consumer<T>>();
+        readonly FasterListThreadSafe<Consumer<T>> _consumers = new FasterListThreadSafe<Consumer<T>>();
     }
 
     public struct Consumer<T> : IDisposable where T : unmanaged, IEntityStruct
