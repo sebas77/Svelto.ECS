d682833cea41e4a8f80f64bfd8560ddb84d4287c
Sebastiano Mandala <ssense@tiscali.it>
2020-01-03 13:55:48 +0000

    Move Svelto.Services on its own repository

--- Svelto.Services/Experimental/IServiceEventContainerFactory.cs
+++
@@ -1,8 +0,0 @@
-
-namespace Svelto.ServiceLayer.Experimental
-{
-    interface IServiceEventContainerFactory
-    {
-        IServiceEventContainer Create();
-    }
-}
--- Svelto.Services/IServiceRequestsFactory.cs
+++
@@ -1,8 +0,0 @@
-namespace Svelto.ServiceLayer
-{
-	public interface IServiceRequestsFactory
-	{
-		RequestInterface Create<RequestInterface>() where RequestInterface:class, IServiceRequest;
-	}
-}
-
